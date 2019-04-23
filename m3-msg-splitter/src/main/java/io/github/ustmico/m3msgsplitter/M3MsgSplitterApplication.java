ddc7bfb82492a863dd4c5457cd8e5594ccb5fc38
wagnerdk <18245675+wagnerdk@users.noreply.github.com>
2019-04-23 13:50:59 +0200

    Add Spring Boot project

---
+++ m3-msg-splitter/src/main/java/io/github/ustmico/m3msgsplitter/M3MsgSplitterApplication.java
@@ -0,0 +1,13 @@
+package io.github.ustmico.m3msgsplitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M3MsgSplitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M3MsgSplitterApplication.class, args);
+	}
+
+}
---
+++ m3-msg-splitter/src/test/java/io/github/ustmico/m3msgsplitter/M3MsgSplitterApplicationTests.java
@@ -0,0 +1,16 @@
+package io.github.ustmico.m3msgsplitter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class M3MsgSplitterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
