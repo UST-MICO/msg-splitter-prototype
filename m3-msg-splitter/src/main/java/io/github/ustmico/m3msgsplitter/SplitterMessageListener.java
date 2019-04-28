00a32200529c02b8e25d8b6184a0d0dff32c605e
wagnerdk <18245675+wagnerdk@users.noreply.github.com>
2019-04-28 17:47:49 +0200

    Add advanced splitting option using Groovy

--- m3-msg-splitter/src/main/java/io/github/ustmico/m3msgsplitter/SplitterMessageListener.java
+++ m3-msg-splitter/src/main/java/io/github/ustmico/m3msgsplitter/SplitterMessageListener.java
@@ -10,8 +10,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
-
+import groovy.lang.Binding;
+import groovy.lang.GroovyShell;
+import groovy.lang.Script;
 import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.util.List;
 import java.util.Optional;
 
 @Slf4j
@@ -41,6 +45,11 @@ public class SplitterMessageListener {
     @Value(value = "${kafka.simpleMode}")
     private boolean simpleMode;
 
+    @Value(value = "${splitter.groovyScript}")
+    private String groovyScript;
+
+    public static final ObjectMapper objectMapper = new ObjectMapper();
+
     @KafkaListener(topics = "${kafka.inputTopic}")
     public void receive(CloudEvent<JsonNode> cloudEvent) {
         log.info("received payload='{}'", cloudEvent);
@@ -48,27 +57,47 @@ public class SplitterMessageListener {
             JsonNode payload = cloudEvent.getData().get();
             log.info("Payload:" + payload);
             log.info(cloudEvent.getData().get().getClass().toString());
-            if (simpleMode) {
+            if (false) {
                 if (payload.isArray()) {
                     for (final JsonNode jsonPart : payload) {
-                        log.info("Send message:" + jsonPart.toString());
-                        final CloudEvent<JsonNode> cloudEventPart = new CloudEventBuilder<JsonNode>()
-                                .type(cloudEvent.getType())
-                                .id(cloudEvent.getId())
-                                .source(cloudEvent.getSource())
-                                .data(jsonPart)
-                                .time(cloudEvent.getTime().get())
-                                .contentType(cloudEvent.getContentType().get())
-                                .build();
-                        log.info("Build CloudEvent:" + cloudEventPart.toString());
-                        kafkaTemplate.send(outputTopic, cloudEventPart);
-
+                        sendMessagePart(cloudEvent, jsonPart);
                     }
                 } else {
                     log.error("Could not split the message:" + payload.toString());
                     //TODO implement invalid message topic
                 }
+            }else{
+                try {
+                    Binding binding = new Binding();
+                    binding.setVariable("cloudEvent", cloudEvent);
+                    binding.setVariable("jsonData", objectMapper.writeValueAsString(payload));
+                    log.debug("Using groovyScript:"+groovyScript);
+                    GroovyShell shell = new GroovyShell(binding);
+                    List<String> result = (List<String>) shell.evaluate(groovyScript);
+                    for(final String jsonPart : result){
+                        JsonNode jsonNode = objectMapper.readTree(jsonPart);
+                        sendMessagePart(cloudEvent, jsonNode);
+                    }
+                } catch (JsonProcessingException e) {
+                    log.error("Could not get valid json from payload",e);
+                } catch (IOException e) {
+                    log.error("Could not get valid json groovy script",e);
+                }
             }
         }
+    }
+
+    private void sendMessagePart(CloudEvent<JsonNode> cloudEvent, JsonNode jsonPart) {
+        log.info("Building message with content:" + jsonPart.toString());
+        final CloudEvent<JsonNode> cloudEventPart = new CloudEventBuilder<JsonNode>()
+                .type(cloudEvent.getType())
+                .id(cloudEvent.getId())
+                .source(cloudEvent.getSource())
+                .data(jsonPart)
+                .time(cloudEvent.getTime().get())
+                .contentType(cloudEvent.getContentType().get())
+                .build();
+        log.info("Build CloudEvent:" + cloudEventPart.toString());
+        kafkaTemplate.send(outputTopic, cloudEventPart);
     }
 }
