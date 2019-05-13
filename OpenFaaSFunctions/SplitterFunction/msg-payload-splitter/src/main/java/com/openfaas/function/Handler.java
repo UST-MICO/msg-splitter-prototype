0a895224c44a455100188687b949bf730996a142
wagnerdk <18245675+wagnerdk@users.noreply.github.com>
2019-05-13 20:58:58 +0200

    Add logging to OpenFaaS function

--- OpenFaaSFunctions/SplitterFunction/msg-payload-splitter/src/main/java/com/openfaas/function/Handler.java
+++ OpenFaaSFunctions/SplitterFunction/msg-payload-splitter/src/main/java/com/openfaas/function/Handler.java
@@ -6,18 +6,23 @@ import com.openfaas.model.IRequest;
 import com.openfaas.model.Response;
 import groovy.lang.Binding;
 import groovy.lang.GroovyShell;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Handler implements com.openfaas.model.IHandler {
 	
+	Logger log = LoggerFactory.getLogger(this.getClass().getName());
 	//Move to environment variable
 	private static final String groovyScript = "import groovy.json.JsonSlurper\r\nimport groovy.json.JsonOutput\r\n\r\nprint jsonData\r\ndef jsonSlurper = new JsonSlurper()\r\ndef jsonObject = jsonSlurper.parseText(jsonData)\r\ndef items = [];\r\njsonObject.items.each{ \r\n    item -> item.orderNumber = jsonObject.orderNumber\r\n    items.add(item)\r\n}\r\nprintln items\r\nreturn JsonOutput.toJson(items)";
 
     public IResponse Handle(IRequest req) {
 		String jsonBody = req.getBody();
+		log.info("Request Body: {}",jsonBody);
 		Binding binding = new Binding();
         binding.setVariable("jsonData", jsonBody);
 		GroovyShell shell = new GroovyShell(binding);
 		String result = (String) shell.evaluate(groovyScript);
+		log.info("Result is:" + result);
         Response res = new Response();
 	    res.setBody(result);
 	    return res;
